34cd4dbdc19bcfee6f154b685e8ebbcf6f70952c
NTHIEU <hieu.nguyen@ant-learning.com>
2026-06-18 18:10:41 +0700

    update Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 
 import "./globals.css";
 import { profile } from "@/data/profile";
@@ -50,6 +51,7 @@ export default function RootLayout({
     <html lang="vi">
       <body>
         <ThemeRegistry>{children}</ThemeRegistry>
+        <Analytics />
       </body>
     </html>
   );
